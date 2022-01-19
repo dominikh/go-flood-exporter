48c4ca7d79f8f10864af261b4b4ac9a9da56ad69
Dominik Honnef <dominik@honnef.co>
2022-01-19 09:07:48 +0100

    Collect per-tracker statistics

--- main.go
+++ main.go
@@ -13,6 +13,7 @@ import (
 	"log"
 	"net/http"
 	"net/http/cookiejar"
+	"strings"
 
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
@@ -52,83 +53,136 @@ func sse(r *bufio.Reader) (event string, data []byte, err error) {
 	}
 }
 
-type transferSummary struct {
-	UpTotal   uint64 `json:"upTotal"`
-	DownTotal uint64 `json:"downTotal"`
-}
-
-type Status struct {
-	Checking    int `json:"checking"`
-	Seeding     int `json:"seeding"`
-	Complete    int `json:"complete"`
-	Downloading int `json:"downloading"`
-	Stopped     int `json:"stopped"`
-	Error       int `json:"error"`
-	Inactive    int `json:"inactive"`
-	Active      int `json:"active"`
-}
-
-type taxonomy struct {
-	StatusCounts Status `json:"statusCounts"`
-}
-
 type Collector struct {
-	client *Client
-
-	byActivity   *prometheus.Desc
-	byCompletion *prometheus.Desc
-	byStatus     *prometheus.Desc
-	byError      *prometheus.Desc
-	in           *prometheus.Desc
-	out          *prometheus.Desc
+	client     *Client
+	trackerMap map[string]string
 }
 
-func NewCollector(address, username, password string) *Collector {
-	return &Collector{
-		client: NewClient(address, username, password),
+type torrent struct {
+	DownTotal   uint64   `json:"downTotal"`
+	UpTotal     uint64   `json:"upTotal"`
+	SizeBytes   uint64   `json:"sizeBytes"`
+	TrackerURIs []string `json:"trackerURIs"`
+	Status      []string `json:"status"`
+}
 
-		byActivity:   prometheus.NewDesc("flood_torrents_by_activity", "", []string{"status"}, nil),
-		byCompletion: prometheus.NewDesc("flood_torrents_by_completion", "", []string{"status"}, nil),
-		byStatus:     prometheus.NewDesc("flood_torrents_by_status", "", []string{"status"}, nil),
-		byError:      prometheus.NewDesc("flood_torrents_by_error", "", []string{"status"}, nil),
-		in:           prometheus.NewDesc("flood_in_bytes", "", nil, nil),
-		out:          prometheus.NewDesc("flood_out_bytes", "", nil, nil),
+func tracker(mapping map[string]string, uris []string) (string, bool) {
+	for _, uri := range uris {
+		if t, ok := mapping[uri]; ok {
+			return t, true
+		}
+	}
+	return "", false
+}
+func NewCollector(address, username, password string, trackerMap map[string]string) *Collector {
+	return &Collector{
+		client:     NewClient(address, username, password),
+		trackerMap: trackerMap,
 	}
 }
 
 func (c *Collector) Describe(ch chan<- *prometheus.Desc) {
-	ch <- c.byActivity
-	ch <- c.byCompletion
-	ch <- c.byStatus
-	ch <- c.byError
-	ch <- c.in
-	ch <- c.out
+	var (
+		total      = prometheus.NewGaugeVec(prometheus.GaugeOpts{Name: "flood_torrents_total"}, []string{"tracker"})
+		statuses   = prometheus.NewGaugeVec(prometheus.GaugeOpts{Name: "flood_torrents_by_status"}, []string{"tracker", "status"})
+		activity   = prometheus.NewGaugeVec(prometheus.GaugeOpts{Name: "flood_torrents_by_activity"}, []string{"tracker", "status"})
+		completion = prometheus.NewGaugeVec(prometheus.GaugeOpts{Name: "flood_torrents_by_completion"}, []string{"tracker", "status"})
+		errors     = prometheus.NewGaugeVec(prometheus.GaugeOpts{Name: "flood_torrents_by_error"}, []string{"tracker", "status"})
+		size       = prometheus.NewGaugeVec(prometheus.GaugeOpts{Name: "flood_torrents_size_bytes"}, []string{"tracker"})
+		in         = prometheus.NewCounterVec(prometheus.CounterOpts{Name: "flood_in_bytes"}, []string{"tracker"})
+		out        = prometheus.NewCounterVec(prometheus.CounterOpts{Name: "flood_out_bytes"}, []string{"tracker"})
+	)
+	total.Describe(ch)
+	statuses.Describe(ch)
+	activity.Describe(ch)
+	completion.Describe(ch)
+	errors.Describe(ch)
+	size.Describe(ch)
+	in.Describe(ch)
+	out.Describe(ch)
 }
 
 func (c *Collector) Collect(ch chan<- prometheus.Metric) {
-	status, traffic, err := c.client.Fetch()
+	torrents, err := c.client.Fetch()
 	if err != nil {
 		ch <- prometheus.NewInvalidMetric(prometheus.NewInvalidDesc(err), err)
 		return
 	}
 
-	total := status.Active + status.Inactive
-	ch <- prometheus.MustNewConstMetric(c.byActivity, prometheus.GaugeValue, float64(status.Active), "active")
-	ch <- prometheus.MustNewConstMetric(c.byActivity, prometheus.GaugeValue, float64(status.Inactive), "inactive")
+	var (
+		total      = prometheus.NewGaugeVec(prometheus.GaugeOpts{Name: "flood_torrents_total"}, []string{"tracker"})
+		statuses   = prometheus.NewGaugeVec(prometheus.GaugeOpts{Name: "flood_torrents_by_status"}, []string{"tracker", "status"})
+		activity   = prometheus.NewGaugeVec(prometheus.GaugeOpts{Name: "flood_torrents_by_activity"}, []string{"tracker", "status"})
+		completion = prometheus.NewGaugeVec(prometheus.GaugeOpts{Name: "flood_torrents_by_completion"}, []string{"tracker", "status"})
+		errors     = prometheus.NewGaugeVec(prometheus.GaugeOpts{Name: "flood_torrents_by_error"}, []string{"tracker", "status"})
+		size       = prometheus.NewGaugeVec(prometheus.GaugeOpts{Name: "flood_torrents_size_bytes"}, []string{"tracker"})
+		in         = prometheus.NewCounterVec(prometheus.CounterOpts{Name: "flood_in_bytes"}, []string{"tracker"})
+		out        = prometheus.NewCounterVec(prometheus.CounterOpts{Name: "flood_out_bytes"}, []string{"tracker"})
+	)
 
-	ch <- prometheus.MustNewConstMetric(c.byCompletion, prometheus.GaugeValue, float64(status.Complete), "complete")
-	ch <- prometheus.MustNewConstMetric(c.byCompletion, prometheus.GaugeValue, float64(total-status.Complete), "incomplete")
+	for _, t := range c.trackerMap {
+		total.WithLabelValues(t).Set(0)
+		size.WithLabelValues(t).Set(0)
+		in.WithLabelValues(t).Add(0)
+		out.WithLabelValues(t).Add(0)
 
-	ch <- prometheus.MustNewConstMetric(c.byStatus, prometheus.GaugeValue, float64(status.Checking), "checking")
-	ch <- prometheus.MustNewConstMetric(c.byStatus, prometheus.GaugeValue, float64(status.Seeding), "seeding")
-	ch <- prometheus.MustNewConstMetric(c.byStatus, prometheus.GaugeValue, float64(status.Downloading), "downloading")
-	ch <- prometheus.MustNewConstMetric(c.byStatus, prometheus.GaugeValue, float64(status.Stopped), "stopped")
+		for _, status := range []string{"checking", "downloading", "seeding", "stopped"} {
+			statuses.WithLabelValues(t, status).Set(0)
+		}
 
-	ch <- prometheus.MustNewConstMetric(c.byError, prometheus.GaugeValue, float64(status.Error), "error")
-	ch <- prometheus.MustNewConstMetric(c.byError, prometheus.GaugeValue, float64(total-status.Error), "no error")
+		activity.WithLabelValues(t, "active").Set(0)
+		activity.WithLabelValues(t, "inactive").Set(0)
 
-	ch <- prometheus.MustNewConstMetric(c.in, prometheus.CounterValue, float64(traffic.DownTotal))
-	ch <- prometheus.MustNewConstMetric(c.out, prometheus.CounterValue, float64(traffic.UpTotal))
+		completion.WithLabelValues(t, "complete").Set(0)
+		completion.WithLabelValues(t, "incomplete").Set(0)
+
+		errors.WithLabelValues(t, "error").Set(0)
+		errors.WithLabelValues(t, "no error").Set(0)
+	}
+
+	for _, torr := range torrents {
+		t, _ := tracker(c.trackerMap, torr.TrackerURIs)
+		total.WithLabelValues(t).Inc()
+
+		complete := false
+		hasError := false
+		for _, status := range torr.Status {
+			switch status {
+			case "active", "inactive":
+				activity.WithLabelValues(t, status).Inc()
+			case "checking", "downloading", "seeding", "stopped":
+				statuses.WithLabelValues(t, status).Inc()
+			case "complete":
+				complete = true
+			case "error":
+				hasError = true
+			}
+		}
+
+		if complete {
+			completion.WithLabelValues(t, "complete").Inc()
+		} else {
+			completion.WithLabelValues(t, "incomplete").Inc()
+		}
+		if hasError {
+			errors.WithLabelValues(t, "error").Inc()
+		} else {
+			errors.WithLabelValues(t, "no error").Inc()
+		}
+
+		in.WithLabelValues(t).Add(float64(torr.DownTotal))
+		out.WithLabelValues(t).Add(float64(torr.UpTotal))
+		size.WithLabelValues(t).Add(float64(torr.SizeBytes))
+	}
+
+	total.Collect(ch)
+	statuses.Collect(ch)
+	activity.Collect(ch)
+	completion.Collect(ch)
+	errors.Collect(ch)
+	size.Collect(ch)
+	in.Collect(ch)
+	out.Collect(ch)
 }
 
 type Client struct {
@@ -151,7 +205,7 @@ func NewClient(address, username, password string) *Client {
 	}
 }
 
-func (c *Client) Fetch() (*Status, *transferSummary, error) {
+func (c *Client) Fetch() (map[string]torrent, error) {
 	payload := struct {
 		Username string `json:"username"`
 		Password string `json:"password"`
@@ -164,45 +218,39 @@ func (c *Client) Fetch() (*Status, *transferSummary, error) {
 	body := bytes.NewReader(b)
 	resp, err := c.client.Post(c.address+"/api/auth/authenticate", "application/json", body)
 	if err != nil {
-		return nil, nil, err
+		return nil, err
 	}
 	defer resp.Body.Close()
 	if cat := resp.StatusCode / 100; cat == 4 || cat == 5 {
-		return nil, nil, errors.New("unexpected status code")
+		return nil, errors.New("unexpected status code")
 	}
 	resp, err = c.client.Get(c.address + "/api/activity-stream")
 	if err != nil {
-		return nil, nil, err
+		return nil, err
 	}
 	defer resp.Body.Close()
-	br := bufio.NewReader(resp.Body)
-	var traffic *transferSummary
-	var status *taxonomy
+	// OPT this buffer is absurdly big to work around our poor approach to parsing server-sent events. For our use-case
+	// it doesn't matter; this function gets called once every 15 seconds, the program is doing nothing else, and it is
+	// running on an oversized server.
+	br := bufio.NewReaderSize(resp.Body, 1024*1024*5)
 	for {
 		event, data, err := sse(br)
 		if err == io.EOF {
-			return nil, nil, errors.New("protocol error")
+			return nil, errors.New("protocol error")
 		}
 		if err == errTooLong {
 			// we don't care about these, for now
 			continue
 		}
 		if err != nil {
-			return nil, nil, err
+			return nil, err
 		}
-		switch event {
-		case "TRANSFER_SUMMARY_FULL_UPDATE":
-			if err := json.Unmarshal(data, &traffic); err != nil {
-				return nil, nil, err
+		if event == "TORRENT_LIST_FULL_UPDATE" {
+			torrents := map[string]torrent{}
+			if err := json.Unmarshal(data, &torrents); err != nil {
+				return nil, err
 			}
-		case "TAXONOMY_FULL_UPDATE":
-			if err := json.Unmarshal(data, &status); err != nil {
-				return nil, nil, err
-			}
-		}
-		if traffic != nil && status != nil {
-			// We got all fields we care about
-			return &status.StatusCounts, traffic, nil
+			return torrents, nil
 		}
 	}
 }
@@ -211,10 +259,11 @@ var (
 	telemetryAddr = flag.String("telemetry.addr", ":4100", "address for Flood exporter")
 	metricsPath   = flag.String("telemetry.path", "/metrics", "URL path for surfacing collected metrics")
 
-	floodRPC      = flag.String("flood.rpc", "", "URL of Flood instance")
-	floodUser     = flag.String("flood.user", "", "Flood username")
-	floodPass     = flag.String("flood.pass", "", "Flood password")
-	floodPassFile = flag.String("flood.pass-file", "", "File to read Flood password from")
+	floodRPC        = flag.String("flood.rpc", "", "URL of Flood instance")
+	floodUser       = flag.String("flood.user", "", "Flood username")
+	floodPass       = flag.String("flood.pass", "", "Flood password")
+	floodPassFile   = flag.String("flood.pass-file", "", "File to read Flood password from")
+	floodTrackerMap = flag.String("flood.tracker-map", "", "A mapping of tracker domains to tracker names, in the format 'example.com=EX foobar.com=FOO'")
 )
 
 func main() {
@@ -234,7 +283,13 @@ func main() {
 		pass = string(bytes.TrimRight(b, "\n"))
 	}
 
-	prometheus.MustRegister(NewCollector(*floodRPC, *floodUser, pass))
+	trackerMap := map[string]string{}
+	for _, m := range strings.Fields(*floodTrackerMap) {
+		p := strings.SplitN(m, "=", 2)
+		trackerMap[p[0]] = p[1]
+	}
+
+	prometheus.MustRegister(NewCollector(*floodRPC, *floodUser, pass, trackerMap))
 	http.Handle(*metricsPath, promhttp.Handler())
 	if err := http.ListenAndServe(*telemetryAddr, nil); err != nil {
 		log.Fatal(err)
